8116c6a6a28a9c0acf9e789f17de4858e3471182
Thiromi97 <120582852+Thiromi97@users.noreply.github.com>
2023-02-12 12:44:58 +0530

    admin style update

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <link rel='stylesheet' href='admin_style.css'>
     <!-- Boxicons CSS -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel='stylesheet' href='admin_style.css'>
 </head>
 
 <body>
