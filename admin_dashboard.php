62109b71ef390f99e02f69108ddb16e79e9bcb88
Thiromi97 <120582852+Thiromi97@users.noreply.github.com>
2023-02-12 09:28:26 +0530

    admin dashboard code

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -4,9 +4,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin_style.css">
+    <!-- Boxicons CSS -->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Admin Dashboard</title>
 </head>
 <body>
-    
+    <div class="sidebar">
+        <div class="logo-details">
+        <i class='bx bxs-diamond'></i>
+        <span class="logo_name">ASIAN ENGINNERS</span>
+        </div>
+    </div>
+    <ul class="nav-links">
+        <li>
+            <a href="#">
+            <i class='bx bx-home-alt'></i>
+             <span class="link_name">Home</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+            <i class='bx bx-outline'></i>
+             <span class="link_name">Scaffolding</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+            <i class='bx bx-cuboid'></i>
+             <span class="link_name">Reinforcement</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+            <i class='bx bx-shape-square'></i>
+             <span class="link_name">Reinforcement Estimate</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+            <i class='bx bx-cog' ></i>
+             <span class="link_name">Settings</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+            <i class='bx bx-log-out'></i>
+             <span class="link_name">Logout</span>
+            </a>
+        </li>
+    </ul>
 </body>
 </html>
